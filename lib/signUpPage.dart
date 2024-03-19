5cc9782f5faeeb7c9df8adefb8a2cee7c3f9d08c
sachithrakalani <kalanisathyangi@gmail.com>
2024-03-19 11:01:22 +0530

    Create Sign Up page

---
+++ lib/signUpPage.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:street_food/homePage.dart';
+
+class SignUpPage extends StatefulWidget {
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title:Text(
+          'Sign Up',style: TextStyle(
+            fontWeight:FontWeight.bold,
+            fontSize: 25 
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body:Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      fontSize: 25,fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  TextField(
+                      //controller: usernameController,
+                      decoration: InputDecoration(
+                        labelText: 'Enter Username',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      //controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Enter your Email',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextField(
+                      //controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Enter your Password',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    GestureDetector(
+                    onTap: () {
+                      //signUp();
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Already have an account?",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context)=>HomePage())
+                          );
+                        },
+                        child: const Text(
+                          'Log in',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
